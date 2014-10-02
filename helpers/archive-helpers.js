194b5ff9580ca738698b5b09fac59cd6ce3c98cf
jasonerpenbeck <jason.erpenbeck@gmail.com>
2014-10-02 12:27:50 -0700

    Web worker reading sites.txt and poised to do more

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -11,7 +11,8 @@ var _ = require('underscore');
 exports.paths = {
   'siteAssets' : path.join(__dirname, '../web/public/'),
   'archivedSites' : path.join(__dirname, '../archives/sites/'),
-  'list' : path.join(__dirname, '../archives/sites.txt')
+  'list' : path.join(__dirname, '../archives/sites.txt'),
+  'fetched': path.join(__dirname, '../archives/sites-fetched.txt')
 };
 
 // Used for stubbing paths for jasmine tests, do not modify
@@ -24,18 +25,12 @@ exports.initialize = function(pathsObj){
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-exports.readListOfUrls = function(file){
-  var listOfURLs;
+exports.readListOfUrls = function(file, callback){
 
-  fs.readFile(file, function(err, data){
-    if(err){
-      console.log(err);
-    } else {
-      listOfURLS = data;
-    }
+  fs.readFile(file, 'utf8',function(err, data){
+    callback(err, data);
   });
 
-  return listOfURLs;
 };
 
 exports.isUrlInList = function(content, matchTo){
--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,34 @@
 // eventually, you'll have some code here that uses the code in `archive-helpers.js`
 // to actually download the urls you want to download.
+
+
+var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+// var url = require('url');
+// var path = require('path');
+// var headers = require('./http-helpers');
+
+
+
+archive.readListOfUrls(archive.paths.list, function(err, data){
+  if(err){
+    console.log("ERROR", err);
+  } else {
+    // console.log(JSON.parse(data));
+
+    var arrayOfSites = data.trim().split('\n');
+
+    for(var i = 0; i < arrayOfSites.length; i++) {
+      console.log('After lunch, we will fetch: ',arrayOfSites[i]);
+    }
+
+
+    fs.appendFile(archive.paths.fetched, data, function(err){
+          if(err){
+            console.log(err);
+          }
+    });
+  }
+});
+
+
