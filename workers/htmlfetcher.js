2b83729758da4b17ea33ffab882a51e3b0f8aed5
jasonerpenbeck <jason.erpenbeck@gmail.com>
2014-10-02 17:31:36 -0700

    CRON WORKS!!!

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -8,30 +8,11 @@ archive.readListOfUrls(archive.paths.list, function(err, data){
   if(err){
     console.log("ERROR", err);
   } else {
-    // console.log(JSON.parse(data));
 
     var arrayOfSites = data.trim().split('\n');
 
     for(var i = 0; i < arrayOfSites.length; i++) {
-      console.log('After lunch, we will fetch: ',arrayOfSites[i]);
-
-      var dest = archive.paths.archivedSites + arrayOfSites[i];
-
-      archive.downloadUrls(arrayOfSites[i], dest);
-
-      // archive.downloadUrls(arrayOfSites[i], dest , function(err,data) {
-      //   if(err) {
-      //     console.log('callback error within Fetcher',err);
-      //   } else {
-      //     fs.appendFile(dest, data , function(err) {
-      //       if(err) {
-      //         console.log('Append File Error within downloadUrls function: ',err);
-      //       } else {
-      //         console.log('Write something to: ',dest);
-      //       }
-      //     });
-      //   }
-      // });
+      archive.downloadUrls(arrayOfSites[i], archive.paths.archivedSites + arrayOfSites[i]);
     }
   }
 });
